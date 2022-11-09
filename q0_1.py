db6ccc6665bef17fe8a855730b8e4d672f63c2de
Joaron4 <82114258+Joaron4@users.noreply.github.com>
2022-11-09 09:05:01 -0500

    Add files via upload

---
+++ q0_1.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q0_1', 'points': None, 'suites': [{'cases': [{'code': '>>> assert verdadero == True\n', 'hidden': True, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
