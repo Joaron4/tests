be3b9836ecbedb2208c7e008dc36a60505f24cb7
Joaron4 <82114258+Joaron4@users.noreply.github.com>
2022-11-09 10:10:45 -0500

    Delete q0_1.py

--- tests/q0_1.py
+++
@@ -1,3 +0,0 @@
-OK_FORMAT = True
-
-test = {'name': 'q0_1', 'points': None, 'suites': [{'cases': [{'code': '>>> assert verdadero == True\n', 'hidden': True, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
